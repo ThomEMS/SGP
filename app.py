fbac83b965ef4aaf9d3ab8c72b72a93feec3b574
Thomeeng <thomas.pelletier@ems-ing.com>
2025-10-22 11:18:13 -0400

    a

--- app.py
+++ app.py
@@ -214,6 +214,10 @@ def ajout():
 
     return render_template('ajout.html', user=user, expenses=expenses)
 
+@app.route("/ping")
+def ping():
+    return {"status": "ok"}, 200
+
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
